3dcfa2f1b17ec8a0878789e0283106e21dff8e24
Yuval Shamir <yuvals2008@gmail.com>
2025-02-03 14:17:02 +0200

    Class Templates

---
+++ Weapon.py
@@ -0,0 +1,20 @@
+class Weapon:
+
+    def __init__(self, range, damage, ammo, hitDelay, RD, maxAmmo):
+        self.range = range
+        self.damage = damage
+        self.maxAmmo = maxAmmo
+        self.hitDelay = hitDelay
+        self.RD = RD
+        self.ammo = ammo
+
+    def reload(ammo, RD, maxAmmo):
+
+
+        return ammo
+
+    
+    def hit(range, damage, ammo, hitDlelay):
+        
+
+        return ammo
