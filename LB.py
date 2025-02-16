1cdd484cef8c288ebf1c994f4296ef2d340d730f
Daniel Shefer <dandanshefer@gmail.com>
2025-02-16 20:37:40 +0200

    added partly the func to move servers

--- LB.py
+++ LB.py
@@ -14,13 +14,23 @@ def packet_info(packet):
         print("Error: Could not decode packet")                       
         return None 
 
+def MoveServer(packet_info, server_borders) -> dict:
+    count = 1
+    if (packet_info['server' + count] == 1):
+        if (server_borders['server' + count][0] >= packet_info['x' + count]):
+            if (server_borders['server' + count])
+
+        
+
+
 def handle_packet(packet):
     if packet.haslayer(scapy.UDP):
         if packet[scapy.UDP].dport == LB_PORT:
             if packet[scapy.UDP].sport not in SERVER_PORTS:
                 return
-    
-    packet_info = packet_info(packet)
+            server_borders = {'server1' : (1 , 1), 'server2' : (1, 1), 'server3' : (1, 1), 'server4' : (1, 1)}
+            packet_info = packet_info(packet)
 
+    
 if __name__ == "__main__":
     main()
