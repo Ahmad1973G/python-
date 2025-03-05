7d06314a39ff0da4b4398426d828388ded83b808
pushos1122 <112702157+pushos1122@users.noreply.github.com>
2025-03-05 10:17:55 +0200

    DB and DOCKERFILE

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
+
+import sqlite3
 conn = sqlite3.connect('players.db')
 
 c = conn.cursor()
 
 c.execute("""CREATE TABLE players (
-          
 
-
-          Player dict = {PlayerID INTGER: {PlayerModel INTEGER: null, PlayerLifecount INTEGER: null,
+          PlayerDict = {PlayerID: {PlayerModel: null, PlayerLifecount: null,
                         PlayerMoney INTEGER: null, Playerslot1 INTEGER: null,
                         Playerslot2 INTEGER: null, Playerslot3 INTEGER: null,
                         Playerslot4 INTEGER: null, Playerslot5 INTEGER: null}}
