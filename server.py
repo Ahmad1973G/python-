75bb7e9fe5a763e04db7ad778ad6e31db5c41a1b
Daniel Shefer <128846536+danielshefer1@users.noreply.github.com>
2025-03-22 21:09:35 +0200

    switched to the new protocol

--- server.py
+++ server.py
@@ -4,85 +4,44 @@ import threading
 import random
 
 # Configuration
-LB_PORT = 5002  # Replace with the load balancer port
-UDP_PORT = LB_PORT + 1  # Replace with the load balancer UDP port
-SERVER_PORT = 5000  # Let the OS assign a port
+LB_PORT = 5002
+UDP_PORT = LB_PORT + 1
+SERVER_PORT = 5000
 
 # Protocol Messages
 SYN = "SYNC CODE 1"
 SYN_ACK = "SYNC+ACK CODE 1"
 ACK = "ACK CODE 2"
 
+
+def get_ip_address():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
+
 class SubServer:
-    """
-    A class to represent a sub-server that connects to a load balancer and handles client connections.
-
-    Attributes:
-    -----------
-    server_socket : socket.socket
-        TCP socket for server-client communication.
-    lb_socket : socket.socket
-        TCP socket for server-load balancer communication.
-    udp_socket : socket.socket
-        UDP socket for broadcasting and receiving messages.
-    server_address : tuple
-        IP address and port of the server.
-    load_balancer_address : tuple
-        IP address and port of the load balancer.
-    connected_clients : dict
-        Dictionary to store connected clients' addresses and sockets.
-    is_connected_to_lb : bool
-        Flag to check if the server is connected to the load balancer.
-    players_data : dict
-        Dictionary to store player information.
-    id : int
-        Unique identifier for the server.
-    ids : list
-        List of assigned client IDs.
-    updated_elements : dict
-        Dictionary to store updated player data.
-    """
-
     def __init__(self):
-        """
-        Initializes the SubServer with necessary sockets and configurations.
-        """
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Changed to TCP
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.lb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.udp_socket.bind(('0.0.0.0', UDP_PORT))
-        self.server_address = (self.get_ip_address(), SERVER_PORT)
+        self.server_address = (get_ip_address(), SERVER_PORT)
         self.server_socket.bind(self.server_address)
-        self.server_socket.listen()  # Listen for incoming connections
+        self.server_socket.listen()
         self.server_socket.settimeout(5)
         self.load_balancer_address = (0, LB_PORT)
-        self.connected_clients = {}  # Store connected clients' addresses (ID -> ((IP, port), socket))
+        self.connected_clients = {}
         self.is_connected_to_lb = False
-        self.players_data = {}  # Dictionary to store player information (ID -> data))
-        self.udp_socket.settimeout(4)  # Set a timeout for UDP socket
-        self.id = 0
-        self.ids = []
+        self.players_data = {}
+        self.udp_socket.settimeout(4)
+        self.server_id = 0
         self.updated_elements = {}
-
-    def get_ip_address(self):
-        """
-        Gets the IP address of the current machine.
-
-        Returns:
-        --------
-        str
-            The IP address of the current machine.
-        """
-        hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostname)
-        return ip_address
+        self.players_counter = {}
 
     def lb_connect_protocol(self):
-        """
-        Handles the connection protocol with the load balancer.
-        """
-        print("Listening on UDP for load balancer on", self.get_ip_address())
+        print("Listening on UDP for load balancer on", get_ip_address())
         while not self.is_connected_to_lb:
             try:
                 data, _ = self.udp_socket.recvfrom(1024)
@@ -99,19 +58,6 @@ class SubServer:
                 self.lb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def readSYNcLB(self, data):
-        """
-        Reads the SYN message from the load balancer and attempts to connect.
-
-        Parameters:
-        -----------
-        data : bytes
-            The data received from the load balancer.
-
-        Returns:
-        --------
-        bool
-            True if the connection is successful, False otherwise.
-        """
         str_data = data.decode()
         if str_data.startswith(SYN):
             lb_ip, lb_port = str_data.split(" ")[-1].split(",")[0].split(";")[1], int(
@@ -127,96 +73,48 @@ class SubServer:
         return False
 
     def sendSYNCACKLB(self):
-        """
-        Sends the SYN+ACK message to the load balancer.
-        """
         self.lb_socket.send(SYN_ACK.encode())
 
     def recvACKLB(self):
-        """
-        Receives the ACK message from the load balancer.
-
-        Returns:
-        --------
-        bool
-            True if the ACK message is received, False otherwise.
-        """
         data = self.lb_socket.recv(1024).decode()
         if data.startswith(ACK):
             print("Received ACK")
-            self.id = int(data.split(";")[-1])
-            print("ID:", self.id)
+            self.server_id = int(data.split(";")[-1])
+            print("Server ID:", self.server_id)
             return True
         return False
 
     def recvSYNclient_sendSYNACK(self, data, addr):
-        """
-        Receives the SYN message from a client and sends the SYN+ACK message.
-
-        Parameters:
-        -----------
-        data : bytes
-            The data received from the client.
-        addr : tuple
-            The address of the client.
-
-        Returns:
-        --------
-        bool
-            True if the SYN message is received, False otherwise.
-        """
         str_data = data.decode()
         if str_data == "SYNC CODE 69":
             self.udp_socket.sendto(f"SYNC+ACK CODE 69 {self.server_address[0]};{self.server_address[1]}".encode(), addr)
             return True
         return False
 
-    def recvACKclient_sendID(self, data, addr):
-        """
-        Receives the ACK message from a client and sends the ID message.
-
-        Parameters:
-        -----------
-        data : bytes
-            The data received from the client.
-        addr : tuple
-            The address of the client.
-
-        Returns:
-        --------
-        int
-            The assigned client ID.
-        """
+    def sendID(self):
         try:
             conn, addr = self.server_socket.accept()
             print("Accepted connection from client")
-            id = random.randint(1, 1000)
-            while id in self.ids:
-                id = random.randint(1, 1000)
-            self.ids.append(id)
-            conn.send(f"ID CODE 69 {id}".encode())
+            client_id = random.randint(1, 1000)
+            while client_id in self.connected_clients.keys():
+                client_id = random.randint(1, 1000)
+            self.connected_clients[client_id] = (addr, conn)
+            self.players_data[client_id] = {}
+            conn.send(f"ID CODE 69 {client_id}".encode())
             print("Sent ID to client")
-            self.connected_clients[id] = (addr, conn)
-            return id
+            self.connected_clients[client_id] = (addr, conn)
+            return client_id
         except Exception as e:
             print(f"Error accepting connection from client: {e}")
 
     def client_start_protocol(self):
-        """
-        Handles the initial connection protocol with clients.
-
-        Returns:
-        --------
-        int
-            The assigned client ID or -1 if the connection fails.
-        """
         print("Listening for clients")
         try:
             data, addr = self.udp_socket.recvfrom(1024)
             if self.recvSYNclient_sendSYNACK(data, addr):
                 print("Received the SYNC packet successfully")
                 print("Sent the SYNC+ACK packet")
-                return self.recvACKclient_sendID(data, addr)
+                return self.sendID()
             return -1
         except socket.timeout:
             return -1
@@ -225,9 +123,6 @@ class SubServer:
             return -1
 
     def client_connect_protocol(self):
-        """
-        Handles the connection protocol with clients.
-        """
         print("Listening for clients on", self.server_address)
         while self.is_connected_to_lb:
             client_id = self.client_start_protocol()
@@ -241,83 +136,114 @@ class SubServer:
         print("Not listening to clients anymore.")
 
     def handle_client(self, client_id):
-        """
-        Handles communication with a connected client.
-
-        Parameters:
-        -----------
-        client_id : int
-            The ID of the connected client.
-        """
         conn = self.connected_clients[client_id][1]
         client_address = self.connected_clients[client_id][0]
         print(f"Connected to client {client_id} at {client_address}")
         try:
             while True:
                 data = conn.recv(1024)
-                if not data:  # Client disconnected
+                if not data:
                     break
                 message = data.decode()
-                # Process the client message (Update player data and send other player info)
-                self.process_player_data(client_id, message)  # Pass the socket object
+                exit_cond = self.process_player_data(client_id, message)
+                if exit_cond == 1:
+                    break
         except Exception as e:
             print(f"Error handling client {client_address}: {e}")
         finally:
-            # Clean up when client disconnects
             print(f"Client {client_address} disconnected.")
             del self.connected_clients[client_id]
             del self.players_data[client_id]
             conn.close()
 
-    def process_player_data(self, client_id, message: str):
-        """
-        Processes the player data received from a client.
-
-        Parameters:
-        -----------
-        client_id : int
-            The ID of the client.
-        message : str
-            The message received from the client.
-        """
+    def process_move(self, client_id, message: str):
         try:
-            message = json.loads(message)
-            for key in message.keys():
-                self.players_data[client_id][key] = message[key]
-                self.updated_elements[client_id][key] = message[key]
+            x = message.split(';')[0]
+            y = message.split(';')[1]
+            self.updated_elements[client_id]['x'] = x
+            self.updated_elements[client_id]['y'] = y
+            self.players_data[client_id]['x'] = x
+            self.players_data[client_id]['y'] = y
+        except Exception as e:
+            print(f"Error processing move for {client_id}: {e}")
 
-            for key in self.players_data[client_id].keys():
-                if key not in self.updated_elements[client_id].keys():
-                    self.updated_elements[client_id][key] = None
+    def process_shoot(self, client_id, message: str):
+        try:
+            start_x = message.split(';')[0]
+            start_y = message.split(';')[1]
+            end_x = message.split(';')[2]
+            end_y = message.split(';')[3]
+            self.updated_elements[client_id]['shoot'] = (start_x, start_y, end_x, end_y)
+        except Exception as e:
+            print(f"Error processing shoot for {client_id}: {e}")
 
-            # Prepare data of other players to send to the client (excluding the client's own data)
-            other_players_data = []
-            for id, data in self.updated_elements.items():
-                if id != client_id:
-                    copy_data = data.copy()
-                    copy_data["id"] = id
-                    other_players_data.append(copy_data)
+    def process_damage_taken(self, client_id, message: str):
+        try:
+            damage = message
+            self.updated_elements[client_id]['health'] -= damage
+            self.players_data[client_id]['health'] -= damage
+        except Exception as e:
+            print(f"Error processing damage taken for {client_id}: {e}")
 
-            self.updated_elements[client_id] = {} # Clear the updated elements
-            # Convert to JSON string and send
+    def process_power(self, client_id, message: str):
+        try:
+            power = message.split(';')
+            self.updated_elements[client_id]['power'] = power, self.players_data['x'], self.players_data['y']
+        except Exception as e:
+            print(f"Error processing power for {client_id}: {e}")
+
+    def process_request(self, client_id):
+        try:
+            self.players_counter[client_id] += 1
+            if self.players_counter[client_id] == 500:
+                self.connected_clients[client_id][1].send("WARNING".encode())
+                return
+            elif self.players_counter[client_id] == 1000:
+                self.connected_clients[client_id][1].send("KICK".encode())
+                self.connected_clients[client_id][1].close()
+                return 1
+
+            other_players_data = {player_id: data for player_id, data in self.updated_elements.items() if player_id != client_id}
             other_players_data_str = json.dumps(other_players_data)
             self.connected_clients[client_id][1].send(other_players_data_str.encode())
+        except Exception as e:
+            print(f"Error processing request for {client_id}: {e}")
+        finally:
+            return 0
+
+    def process_player_data(self, client_id, message: str):
+        try:
+            self.updated_elements[client_id] = {}
+            if message.startswith("MOVE"):
+                self.process_move(client_id, message.split(" ")[-1])
+            elif message.startswith("SHOOT"):
+                self.process_shoot(client_id, message.split(" ")[-1])
+            elif message.startswith("DAMAGE"):
+                self.process_damage_taken(client_id, message.split(" ")[-1])
+            elif message.startswith("POWER"):
+                self.process_power(client_id, message.split(" ")[-1])
+            elif message.startswith("REQUEST"):
+                self.process_request(client_id)
+
+            if self.updated_elements != {}:
+                self.players_counter[client_id] = 0
+            else:
+                self.players_counter[client_id] = 0
+
+            return 0
         except Exception as e:
             print(f"Error processing player data for {client_id}: {e}")
 
     def run(self):
-        """
-        Runs the sub-server.
-        """
-        # Start load balancer connection
         lb_thread = threading.Thread(target=self.lb_connect_protocol)
         lb_thread.start()
-        lb_thread.join()  # Wait for the thread to establish a connection with the load balancer
+        lb_thread.join()
 
         if self.is_connected_to_lb:
             self.client_connect_protocol()
         else:
             print("Load balancer not properly connected, not listening to clients.")
+
 
 if __name__ == "__main__":
     server = SubServer()
