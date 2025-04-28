48ad55d0bc3e8f064e444315c2988e52fd045609
daniel mantin <dani.mantin@gmail.com>
2025-04-28 14:40:23 +0300

    Add map folder

---
+++ map/lava.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="lava" tilewidth="16" tileheight="16" tilecount="147" columns="49">
+ <image source="Top Down Lava Tileset 16x16 Free/FREE TILESET FILES/Environment ( With Animations )/Burning Lava/spritesheet-burninglava.png" width="784" height="48"/>
+</tileset>
