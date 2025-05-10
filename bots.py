2b55eb2f7a104c4eee980d9452b7e5424be5c7d9
DTG358 <danielassor2901@gmail.com>
2025-05-10 17:48:13 +0300

    finished bot exept dying

--- bots.py
+++ bots.py
@@ -1,6 +1,6 @@
 import time
-from fileinput import close
-#from scipy.spatial import KDTree
+from distutils.command.check import check
+
 import ClientSocket
 import pygame
 import math
@@ -8,7 +8,7 @@ import threading
 
 
 class Bots ():
-    def __init__(self,my_x,my_y,type,closest_x,closest_y,screen):
+    def __init__(self,my_x,my_y,type,closest_x,closest_y,kd_tree,pos_to_tile):
         #type true= long range bot false=short
         #closest_p=0 if no player is close
         self.my_x=my_x
@@ -17,7 +17,7 @@ class Bots ():
         self.moving=True
         self.bot_rect={
             "image": pygame.Surface((60, 60)),
-            "rect": pygame.Rect(500, 325, 60, 60)
+            "rect": pygame.Rect(0,0, 60, 60)
         }
         if type:
             self.bot_range=50000
@@ -29,27 +29,52 @@ class Bots ():
             self.bullet_speed =70
             self.damage = 30
             self.weapon=0
-        self.screen=screen
         self.closest_x=closest_x
         self.Socket = ClientSocket.ClientServer()
         self.closest_y=closest_y
+        self.pos_to_tile=pos_to_tile
+        self.kd_tree= kd_tree
         self.lock=threading.Lock()
         self.new_target=False
         self.thread_movebots=threading.Thread(target=self.move)
         self.thread_movebots.start()
 
+    def check_collision_nearby(self,x,y, radius=80):
+        self.bot_rect['rect'].x=x
+        self.bot_rect['rect'].y=y
+        center = (self.bot_rect['rect'].x, self.bot_rect['rect'].y)
+        nearby_indices = self.kd_tree.query_ball_point(center, radius)
+
+        for idx in nearby_indices:
+            x_c, y_c = self.kd_tree.data[idx]
+            coll_obj_x, coll_obj_w, coll_obj_y, coll_obj_h = self.pos_to_tile[(x_c, y_c)]
+
+            # AABB-style collision check (same logic as your check_collision)
+            if (
+                    self.bot_rect['rect'].x - self.bot_rect['rect'].width / 2 <= coll_obj_x + coll_obj_w and
+                    self.bot_rect['rect'].x + self.bot_rect['rect'].width / 2 >= coll_obj_x and
+                    self.bot_rect['rect'].y - self.bot_rect['rect'].height / 2 <= coll_obj_y and
+                    self.bot_rect['rect'].y + self.bot_rect['rect'].height / 2 >= coll_obj_y - coll_obj_h
+            ):
+                # print(f"Collision with: {coll_obj_x}, {coll_obj_w}, {coll_obj_y}, {coll_obj_h}")
+                return True
+
+        # print("No collision")
+        return False
     def SeNdTArGeT(self,x,y):
-        self.closest_y=y
-        self.closest_x=x
-        self.new_target=True
-        self.moving=True
+        if x!=None and y!=None:
+            self.closest_y=y
+            self.closest_x=x
+            self.new_target=True
+            self.moving=True
+        else:
+            self.moving=False
     def move(self):
         target_x=False
         target_y=False
         while True:
             if self.moving:
                 self.new_target=False
-                col=False
                 t_x = self.closest_x - 500
                 t_y = 325 - self.closest_y
                 m_x = self.my_x - 500 - t_x
@@ -62,8 +87,6 @@ class Bots ():
                 t_x += self.closest_x
                 target_x = False
                 target_y = False
-                hypo_x=self.my_x
-                hypo_y=self.my_y
                 while not self.new_target and (not target_x or not target_y):
                     if int(self.my_x)!=int(t_x):
                         move_x=(t_x-self.my_x)/abs(t_x-self.my_x)
@@ -77,17 +100,8 @@ class Bots ():
                         move_y=0
                     self.my_x+=move_x
                     self.my_y+=move_y
-                    self.bot_rect['rect'].x=self.my_x
-                    self.bot_rect['rect'].y=self.my_y
-                    self.bot_rect['image']=pygame.Surface((30, 30))
-                    self.bot_rect['image'].fill((255, 0, 0))
-                    self.screen.fill((0, 0, 0))
-                    pygame.draw.rect(self.screen,(0,0,255),(500,325,60,60))
-                    self.screen.blit(self.bot_rect['image'], self.bot_rect['rect'])
-                    pygame.display.flip()
                     time.sleep(0.001)
-                    if self.my_x >= (500 - self.bot_rect["rect"].width) and self.my_x <= (500 + self.bot_rect["rect"].width) and self.my_y >= (325 - self.bot_rect["rect"].height) and self.my_y <= (325 + self.bot_rect["rect"].height):
-                        print("col")
+                    if self.check_collision_nearby(self.my_x,self.my_y):
                         option_x = [self.my_x,self.my_x]
                         option_y = [self.my_y,self.my_y]
                         if move_y==0:
@@ -102,28 +116,22 @@ class Bots ():
 
                             option_x[0]+=move_x
                         while True:
-                            if not (option_x[0] >= (500 - self.bot_rect["rect"].width) and option_x[0] <= (500 + self.bot_rect["rect"].width) and option_y[0] >= (325 - self.bot_rect["rect"].height) and option_y[0] <= (325 + self.bot_rect["rect"].height)):
+                            if not self.check_collision_nearby(option_x[0],option_y[0]):
                                 best_route=0
                                 move_y=0
                                 break
 
-                            if not (option_x[1] >= (500 - self.bot_rect["rect"].width) and option_x[1] <= (500 + self.bot_rect["rect"].width) and option_y[1] >= (325 - self.bot_rect["rect"].height) and option_y[1] <= (325 + self.bot_rect["rect"].height)):
+                            if not self.check_collision_nearby(option_x[1],option_y[1]):
                                 best_route=1
                                 move_x=0
                                 break
                             option_y[1] += move_y
                             option_x[0] += move_x
-                        while (self.my_x!=option_x[0] or self.my_y!=option_y[0]) and  (self.my_x!=option_x[1] or self.my_y!=option_y[1]):
+                        while self.my_x!=option_x[best_route] or self.my_y!=option_y[best_route]:
                             self.my_x+=move_x
                             self.my_y+=move_y
-                            self.bot_rect['rect'].x = self.my_x
-                            self.bot_rect['rect'].y = self.my_y
-                            pygame.draw.rect(self.screen, (0, 0, 255), (500, 325, 60, 60))
-                            self.screen.blit(self.bot_rect['image'], self.bot_rect['rect'])
-                            pygame.display.flip()
+
                             time.sleep(0.001)
-                            self.screen.fill((0,0,0))
-                            #pygame.display.flip()
                 if target_x and target_y:
                     self.moving=False
             elif target_x and target_y and not self.new_target:
