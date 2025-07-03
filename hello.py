165cc057576a958d529b7d210887b0cbb4eddc71
Daniel Shefer <128846536+danielshefer1@users.noreply.github.com>
2025-07-03 23:31:24 +0300

    m

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
-for i in range(10):
-    print(i)
+import time
+
+start_time = time.time()
+i = 0
+while i < 1000000:
+    i += 1
+
+end_time = time.time()
+print(f"Execution time: {end_time - start_time} seconds")
+
+a = 0.11202597618103027
+b = 0.0017256
+
+print(a / b)
